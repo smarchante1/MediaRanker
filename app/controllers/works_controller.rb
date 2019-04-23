c2dcc6ac01b94ef7e3d8bff83af2107ff342500c
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 16:14:05 -0700

    added actions for index show and new in works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,42 @@
 class WorksController < ApplicationController
+  def index
+    @works = Works.all
+  end
+
+  def show 
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      flash[:failure] = "Your search returned no results."
+      redirect_to root_path
+    end
+  end
+
+  def new
+    @work = Works.new
+  end
+
+  def create
+   
+  end
+
+  def edit
+
+  end
+
+  def update
+
+
+  end
+
+  def destroy
+
+
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :pub_year, :description)
+  end
 end
