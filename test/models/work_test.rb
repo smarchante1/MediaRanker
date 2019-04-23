155c5a572a873edf76a3246139d9cd9229fe5a85
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 15:53:27 -0700

    completed validation test for the work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:album) }
 
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  describe "validations" do
+    it "must have a title" do
+      work.title = nil
+
+      valid_work = work.valid?
+
+      expect(valid_work).must_equal false
+      expect(work.errors.messages).must_include :title
+      expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it "must have a unique title" do
+      identical_title = Work.new(title: "Lost Roses", creator: "Lemony Snickett")
+
+      expect(identical_title.save).must_equal false
+
+      expect(identical_title.errors.messages).must_include :title
+      expect(identical_title.errors.messages[:title]).must_equal ["has already been taken"]
+    end
+  end
 end
+
+
