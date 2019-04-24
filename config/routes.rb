6fd77aff15fa3038e640df7fdacc7bc5ee56228c
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 22:22:15 -0700

    added main nav view with login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'homepages#index'
 
   resources :works
+
+  get '/login', to: 'users#login_form', as: 'login'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: 'logout'
+  get '/users/current', to: 'users#current', as: 'current_user'
 end
