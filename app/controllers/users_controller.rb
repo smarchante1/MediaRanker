4d3281fda2452841c0eaac55635eb138b5ba3ba6
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 21:55:51 -0700

    added actions for users controllere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+
+    if user 
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back, #{username}."
+      redirect_to root_path
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Welcome new user, #{user.username}."
+      redirect_to root_path
+    end
+  end
+
+  def current
+    @user = User.find_by(id: session[:user_id])
+    unless @user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+    end
+  end
+
+  def log_out
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,5 @@ class Work < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :creator, :category, :description, :pub_year, presence: true
+
 end
