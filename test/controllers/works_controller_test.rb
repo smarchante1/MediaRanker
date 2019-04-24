14a2cfef5846dee615f8c45a3810ca6cb9ee9857
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 18:10:43 -0700

    added corresponding test for show view

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -10,6 +10,26 @@ describe WorksController do
 
       must_respond_with :success
     end
+  end
+
+  describe "show action" do
+    it "shows a details page for an existing work" do 
+      get work_path(work.id)
+
+      must_respond_with :success
+    end
+
+    it "redirects if given invalid id" do
+      get work_path(-1)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+      expect(flash[:failure]).must_equal "Your search returned no results."
+    end
+  end
+
+  describe "new" do
+    it ""
 
 
   end
