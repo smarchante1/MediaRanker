49272e3bf7a8d10fc0b7d2aa77b3b2b3c4d9df28
Stephanie Marchante <smarcha04@gmail.com>
2019-04-23 20:44:18 -0700

    added test for create action

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -29,7 +29,33 @@ describe WorksController do
   end
 
   describe "new" do
+    it "gets to the new page" do
+      get new_work_path
 
+      must_respond_with :success
+    end
+  end
 
+  describe "create" do
+    it "successfully creates a new media" do 
+      media_params = {
+        "work": {
+          category: "book",
+          title: "1984",
+          creator: "George Orwell",
+          pub_year: 1948,
+          description: "A startling and haunting vision of the world, 1984 is so powerful that it is completely convincing from start to finish"
+        }
+      }
+
+      expect {
+        post works_path, params: media_params
+      }.must_change "Work.count", 1
+
+      must_respond_with :redirect
+      new_media = Work.find_by(title: media_params[:work][:title])
+      expect(flash[:success]).must_equal  "#{new_media.title} was successfully created."
+      must_redirect_to work_path(new_media.id)
+    end
   end
 end
