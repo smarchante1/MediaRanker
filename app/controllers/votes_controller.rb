0041492eab55dfde2678ee4c831c54fec5371526
Stephanie Marchante <smarcha04@gmail.com>
2019-04-25 14:47:28 -0700

    adjustments to create vote action

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,20 +4,24 @@ class VotesController < ApplicationController
   end
 
   def create
-    @work = Work.find_by(id: params[:work_id])
-    @user = User.current
+    work = Work.find_by(id: params[:id])
+    user = User.find_by(id: session[:user_id])
+    vote = Vote.new(vote_date: Date.today, user_id: user.id, work_id: work.id )
 
-    @vote = Vote.new(work_id: work.id, user_id: user.id)
-
-    is_successful = @vote.save
+    is_successful = vote.save
 
     if is_successful
       flash[:success] = "Successfully upvoted!"
-      redirect_to works_path
+      redirect_to work_path
     else
       flash[:error] = "You must log in to vote"
-      redirect_to works_path
+      redirect_to work_path
     end
+  end
+  
+  private
 
+  def vote_params
+    return params.require(:vote).permit(:id)
   end
 end
