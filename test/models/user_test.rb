989bb992dd2f3e03d5b34ae2d0e27d71b43a48ff
Stephanie Marchante <smarcha04@gmail.com>
2019-04-28 19:32:26 -0700

    added model tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,4 +7,20 @@ describe User do
     is_valid = user.valid?
     value(is_valid).must_equal true
   end
+
+  it "checks if username is valid" do
+    user_name = user.username
+    
+    expect(user).must_be :valid?
+    expect(user.username).must_equal user_name
+
+  end
+
+  it "can't create a new user without a username" do
+    user = User.new
+    user.username = ""
+    failed_user = user.save
+
+    expect(failed_user).must_equal false
+  end
 end
